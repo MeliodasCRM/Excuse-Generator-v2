993ae12800d936b42076c20cc92ba168c0fc102b
MeliodasCRM <Pabermb@gmail.com>
2024-10-09 08:18:31 +0000

    function optimization

--- src/app.js
+++ src/app.js
@@ -6,35 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  function excusegenerator() {
-    const who = [
+  const excusegenerator = () =>
+    [
       "the point guard",
       "the center",
       "the coach",
       "the star player",
       "the rookie"
-    ];
-    const action = [
+    ][Math.floor(Math.random() * 5)] +
+    " " +
+    [
       "missed an open layup",
       "missed a three",
       "made a bad pass",
       "forgot the play",
       "went to poop"
-    ];
-    const where = [
+    ][Math.floor(Math.random() * 5)] +
+    " " +
+    [
       "at the buzzer",
       "in the final quarter",
       "during overtime",
       "under the basket",
       "on defense"
-    ];
-
-    const randomwho = who[Math.floor(Math.random() * who.length)];
-    const randomaction = action[Math.floor(Math.random() * action.length)];
-    const randomwhere = where[Math.floor(Math.random() * where.length)];
-
-    return `We lost the game because ${randomwho} ${randomaction} ${randomwhere}.`;
-  }
+    ][Math.floor(Math.random() * 5)];
 
   function reloadExcuse() {
     document.getElementById("excusegenerator").innerHTML = excusegenerator();
